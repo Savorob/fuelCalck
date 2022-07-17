f9eadbce1de64b76e29bd2af526202e890225dde
Savorob <savorob@gmail.com>
2022-07-17 20:08:17 +0500

    add foarm clean script

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ let averageFlow = document.querySelector("#average-flow");
 let distance = document.querySelector("#distance");
 let result = document.querySelector('#result');
 const calckBtn = document.querySelector('#calck-btn');
+const calearBtn = document.querySelector("#clear-btn");
 
 calckBtn.addEventListener('click', function(){
     result.innerHTML = ((distance.value / 100) * averageFlow.value).toFixed(2) + " л";
+})
+calearBtn.addEventListener('click', function(){
+    distance.value = "",
+    averageFlow.value = "",
+    result.innerHTML = "";
 })
