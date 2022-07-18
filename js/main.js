0562f8f02da496d6156a2217f72779134d481fa5
Savorob <savorob@gmail.com>
2022-07-18 18:33:23 +0500

    modify calckResult function and add odometr input fiealds

--- js/main.js
+++ js/main.js
@@ -1,14 +1,44 @@
 let averageFlow = document.querySelector("#average-flow");
 let distance = document.querySelector("#distance");
-let result = document.querySelector('#result');
-const calckBtn = document.querySelector('#calck-btn');
+let result = document.querySelector("#result");
+let distanceStart = document.querySelector("#distance-start");
+let distanceFinish = document.querySelector("#distance-finish");
+
+const calckBtn = document.querySelector("#calck-btn");
 const calearBtn = document.querySelector("#clear-btn");
 
-calckBtn.addEventListener('click', function(){
-    result.innerHTML = ((distance.value / 100) * averageFlow.value).toFixed(2) + " л";
-})
-calearBtn.addEventListener('click', function(){
-    distance.value = "",
-    averageFlow.value = "",
-    result.innerHTML = "";
-})
+calckBtn.addEventListener("click", calckResult);
+calearBtn.addEventListener("click", clearForm);
+
+function calckResult() {
+  if (
+    averageFlow.value.length != 0 &&
+    (distance.value.length != 0 ||
+      (distanceFinish.value.length != 0) & (distanceStart.value.length != 0))
+  ) {
+    if (distanceStart.value.length == 0 || distanceFinish.value.length == 0) {
+      result.innerHTML =
+        ((distance.value / 100) * averageFlow.value).toFixed(2) + " л";
+    } else if (
+      distanceStart.value.length != 0 &&
+      distanceFinish.value.length != 0
+    ) {
+      result.innerHTML =
+        (
+          ((distanceFinish.value - distanceStart.value) / 100) *
+          averageFlow.value
+        ).toFixed(2) + " л";
+    } else if (
+      (distanceStart.value.length == 0 || distanceFinish.value.length == 0) &&
+      distance.value.length != 0
+    ) {
+      result.innerHTML =
+        ((distance.value / 100) * averageFlow.value).toFixed(2) + " л";
+    }
+  } else {
+    alert("Заполните поля!");
+  }
+}
+function clearForm() {
+  document.querySelector("form").reset();
+}
